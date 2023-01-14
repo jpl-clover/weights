6618ef5fac091355e7580629d7256ef454ad4f24
Kai Pak <kaipak@users.noreply.github.com>
2023-01-13 21:05:42 -0800

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -2,7 +2,7 @@ import torch
 import torchvision.models
 
 def resnet18_distilled_from_r101_1x_sk0_finetuned_on_100pctMSL():
-    checkpoint_url = "https://github.com/jpl-clover/weights/raw/main/self-supervised-distillation-for-computer-vision-onboard-planetary-robots/resnet18_distilled_from_r101_1x_sk0_finetuned_on_100pctMSL.tar"
+    checkpoint_url = "https://jpl-clover.s3.amazonaws.com/weights/MSL+N5920+E0-100+mobilenetv3_large+LR1e-04+B64+Cdistilled_students_100k-Teacher_simclr_T-Epoch_200_Student_mobilenetv3_large_distill-Epoch_r101_2x_sk1-checkpoint_0199.pth.tar+2023-01-12_192328_checkpoint.tar"
     model = torchvision.models.resnet18()
     model.load_state_dict(torch.hub.load_state_dict_from_url(checkpoint_url, progress=True))
     return model

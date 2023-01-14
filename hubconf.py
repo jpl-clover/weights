131018564ee872c076bc2355ccd89c625e2fed67
Kai Pak <kaipak@users.noreply.github.com>
2023-01-13 16:54:44 -0800

    Update hubconf.py
    Try raw github URL for resnet18_distilled

--- hubconf.py
+++ hubconf.py
@@ -2,7 +2,7 @@ import torch
 import torchvision.models
 
 def resnet18_distilled_from_r101_1x_sk0_finetuned_on_100pctMSL():
-    checkpoint_url = "https://github.com/jpl-clover/weights/blob/main/self-supervised-distillation-for-computer-vision-onboard-planetary-robots/resnet18_distilled_from_r101_1x_sk0_finetuned_on_100pctMSL.tar"
+    checkpoint_url = "https://github.com/jpl-clover/weights/raw/main/self-supervised-distillation-for-computer-vision-onboard-planetary-robots/resnet18_distilled_from_r101_1x_sk0_finetuned_on_100pctMSL.tar"
     model = torchvision.models.resnet18()
     model.load_state_dict(torch.hub.load_state_dict_from_url(checkpoint_url, progress=True))
     return model
